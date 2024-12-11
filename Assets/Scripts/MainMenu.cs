d1a82093f7f80df29e2ffbf4268cbe4b6a5034a9
LeoKumpulainen <lego.kumpulainen@gmail.com>
2024-12-11 09:45:54 +0200

    Level select

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -20,6 +20,46 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadSceneAsync(2);
     }
 
+    public void Level1()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadSceneAsync(4);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadSceneAsync(6);
+    }
+
+    public void Level5()
+    {
+        SceneManager.LoadSceneAsync(7);
+    }
+
+    public void Level1B()
+    {
+        SceneManager.LoadSceneAsync(8);
+    }
+
+    public void Level2B()
+    {
+        SceneManager.LoadSceneAsync(9);
+    }
+
+    public void Level3B()
+    {
+        SceneManager.LoadSceneAsync(10);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
